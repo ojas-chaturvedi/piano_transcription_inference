a06fd9b810e263dff0f2b8a9dfcb0414ffbab837
ojas-chaturvedi <ojaschaturvedi06@gmail.com>
2025-04-02 08:06:59 -0400

    Replace example.py with main.py for AMT competition

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ def inference(args):
     """Inference template.
 
     Args:
-      model_type: str
-      audio_path: str
-      cuda: bool
+        model_type: str
+        audio_path: str
+        cuda: bool
     """
 
     # Arugments & parameters
     audio_path = args.audio_path
     output_midi_path = args.output_midi_path
-    device = 'cuda' if args.cuda and torch.cuda.is_available() else 'cpu'
- 
+    device = "cuda" if args.cuda and torch.cuda.is_available() else "cpu"
+
     # Load audio
     audio, _ = librosa.load(path=audio_path, sr=sample_rate, mono=True)
 
@@ -33,15 +33,15 @@ def inference(args):
     # Transcribe and write out to MIDI file
     transcribe_time = time.time()
     transcribed_dict = transcriptor.transcribe(audio, output_midi_path)
-    print('Transcribe time: {:.3f} s'.format(time.time() - transcribe_time))
+    print("Transcribe time: {:.3f} s".format(time.time() - transcribe_time))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument('--audio_path', type=str, required=True)
-    parser.add_argument('--output_midi_path', type=str, required=True)
-    parser.add_argument('--cuda', action='store_true', default=False)
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("--audio_path", type=str, required=True)
+    parser.add_argument("--output_midi_path", type=str, required=True)
+    parser.add_argument("--cuda", action="store_true", default=False)
 
     args = parser.parse_args()
     inference(args)
